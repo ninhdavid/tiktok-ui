94c378f32aef94763a32560c7057f271fec2a192
NinhNguyen <ninhdavid96@gmail.com>
2023-01-25 01:34:05 +0900

    add route / layout

--- config-overrides.js
+++ config-overrides.js
@@ -4,3 +4,11 @@ module.exports = override(
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	useBabelRc()
 );
+module.exports = function override(config, env) {
+	console.log('React app rewired works!');
+
+	config.resolve.fallback = {
+		fs: false,
+	};
+	return config;
+};
